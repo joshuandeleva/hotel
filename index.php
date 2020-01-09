9f95a5f95f7c6734c719039924e71e4f61287a66
premierboy <48788501+premierboy@users.noreply.github.com>
2020-01-09 12:20:06 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,210 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hotelwebsite</title>
+    <link rel="stylesheet" href="fontawesome-free-5.0.1">
+    <link rel="stylesheet" href="fontawesome-free-5.0.1/css/fontawesome-all.css" type="text/css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="Style.css">
+</head>
+<body>
+<div class="topnav">
+   <div id="serchicon" class="searchbar">
+        <input type="text" placeholder="search meals or drinks">
+       <i class="fa fa-search"></i>
+    </div>
+    <div class="sectionc align-content-end">
+        <div id=""class="">
+            <i id="shpicon"  style="border: 1px solid orange" class="fa fa-shopping-cart"></i>
+            <span style="position: absolute">Ksh2500</span>
+
+
+        </div>
+        <div class="">
+            <i  class="fa fa-user-circle"style="color: #fd7e14"></i>
+            <span style="position: absolute">Nic nganga</span>
+            <i class="fa fa-caret-down "></i>
+
+        </div>
+    </div>
+</div>
+    <!--category section-->
+<div class="mainse">
+           <div class="categoryimag img-thumbnail"style="margin: 25px 10px ">
+            <img src="images/IMG-20200106-WA0010.jpg"width="60px" alt="">
+          <p>breakfast</p>
+           </div>
+            <div class="categoryimag img-thumbnail"style="margin: 25px 10px ">
+                <img src="images/coffee-cup.png"width="60px" alt="">
+                <p>drinks</p>
+            </div>
+            <div class="categoryimag img-thumbnail"style="margin: 25px 10px ">
+                <img src="images/turkey.png"width="60px" alt="">
+                <p>breakfast</p>
+            </div>
+           <div class="categoryimag img-thumbnail"style="margin: 25px 10px ">
+               <img src="images/soft-drink.png"width="60px" alt="">
+               <p>mutton</p>
+           </div>
+            <div class="categoryimag img-thumbnail"style="margin: 25px 10px ">
+                <img src="images/pizza.png"width="60px"alt="">
+                <p>chicken</p>
+            </div>
+           <div class="categoryimag img-thumbnail"style="margin: 25px 10px ">
+                   <img src="images/cheese.png"width="60px"alt="">
+               <p>slides</p>
+            </div>
+             <div class="categoryimag img-thumbnail"style="margin: 25px 10px ">
+                   <img src="images/cupcake.png"width="60px"alt="">
+                 <p>appetizers</p>
+             </div>
+                  <div class="categoryimag img-thumbnail"style="margin: 25px 10px ">
+                      <img src="images/fish.png"width="60px"alt="">
+                      <p>mix</p>
+                  </div>
+                <div class="categoryimag img-thumbnail"style="margin: 25px 10px ">
+                        <img src="images/676020.png"width="60px"alt="">
+                         <p>sweets</p>
+                 </div>
+                <div class="categoryimag img-thumbnail"style="margin: 25px 10px ">
+                       <img src="images/1016464.svg"width="60px"alt="">
+                    <p>kidmeals</p>
+                </div>
+               <div class="categoryimag img-thumbnail"style="margin: 25px 10px ">
+                     <img src="images/pizza.svg"width="60px"alt="">
+                   <p>pizza</p>
+               </div>
+
+</div>
+<!--sectionof the images-->
+<hr >
+<div class="sectioncategoryt">
+    <div class="sectiontitlew">
+        <h2 style="font-size: x-large">Mutton</h2>
+    </div>
+    <div class="sectionimageqw img-thumbnail"style="margin: 25px 10px">
+        <img src="images/WhatsApp%20Image%202020-01-07%20at%2012.45.22.jpeg"width="200px"height="200px"  alt="" content="top">
+        <p><span style="background: orange">Ksh 1000</span></p>
+        <div class="sectioniconse">
+            <p>muttonbakari</p>
+            <i class="fa fa-minus"></i> <span>1</span> <i class="fa fa-plus"></i>
+            <p>+ Add to order</p>
+        </div>
+    </div>
+    <div class="sectionimageqw img-thumbnail"style="margin: 25px 10px">
+        <img src="images/WhatsApp%20Image%202020-01-07%20at%2012.45.21.jpeg"width="200px"height="200px" alt="">
+        <p><span style="background: orange">Ksh 1000</span></p>
+        <div class="sectioniconse">
+            <p>muttonbakari</p>
+            <i class="fa fa-minus"></i> <span>1</span> <i class="fa fa-plus"></i>
+            <p>+ Add to order</p>
+        </div>
+    </div>
+    <div class="sectionimageqw img-thumbnail"style="margin: 25px 10px">
+        <img src="images/WhatsApp%20Image%202020-01-07%20at%2012.45.23(1).jpeg"width="200px"height="200px" alt="">
+        <p><span style="background: orange">Ksh 1000</span></p>
+        <div class="sectioniconse">
+            <p>muttonbakari</p>
+            <i class="fa fa-minus"></i> <span>1</span> <i class="fa fa-plus"></i>
+            <p>+ Add to order</p>
+        </div>
+    </div>
+    <div class="sectionimageqw img-thumbnail"style="margin: 25px 10px">
+        <img src="images/WhatsApp%20Image%202020-01-07%20at%2012.45.23.jpeg"width="200px"height="200px" alt="">
+        <p><span style="background: orange">Ksh 1000</span></p>
+        <div class="sectioniconse">
+            <p>muttonbakari</p>
+            <i class="fa fa-minus"></i> <span>1</span> <i class="fa fa-plus"></i>
+            <p>+ Add to order</p>
+        </div>
+    </div>
+    <div class="sectionimageqw img-thumbnail"style="margin: 25px 10px">
+        <img src="images/WhatsApp%20Image%202020-01-07%20at%2012.45.25.jpeg"width="200px"height="200px" alt="">
+        <p><span style="background: orange">Ksh 1000</span></p>
+        <div class="sectioniconse">
+            <p>muttonbakari</p>
+            <i class="fa fa-minus"></i> <span>1</span> <i class="fa fa-plus"></i>
+            <p>+ Add to order</p>
+        </div>
+    </div>
+    <br><br>
+    <div class="sectionimageqw img-thumbnail"style="margin: 25px 10px">
+        <img src="images/WhatsApp%20Image%202020-01-07%20at%2012.45.25(1).jpeg"width="200px"height="200px" alt="">
+        <p><span style="background: orange">Ksh 1000</span></p>
+        <div class="sectioniconse">
+            <p>muttonbakari</p>
+            <i class="fa fa-minus"></i> <span>1</span> <i class="fa fa-plus"></i>
+            <p>+ Add to order</p>
+        </div>
+    </div>
+    <div class="sectionimageqw img-thumbnail"style="margin: 25px 10px">
+        <img src="image/spaghetti-sandwich.gif"width="200px"height="200px" alt="">
+        <p><span style="background: orange">Ksh 1000</span></p>
+        <div class="sectioniconse">
+            <p>muttonbakari</p>
+            <i class="fa fa-minus"></i> <span>1</span> <i class="fa fa-plus"></i>
+            <p>+ Add to order</p>
+        </div>
+    </div>
+    <div class="sectionimageqw img-thumbnail"style="margin: 25px 10px">
+        <img src="image/Lunch2.jpg"width="200px"height="200px" alt="">
+        <p><span style="background: orange">Ksh 1000</span></p>
+        <div class="sectioniconse">
+            <p>muttonbakari</p>
+            <i class="fa fa-minus"></i> <span>1</span> <i class="fa fa-plus"></i>
+            <p>+ Add to order</p>
+        </div>
+    </div>
+    <div class="sectionimageqw img-thumbnail"style="margin: 25px 10px">
+        <img src="image/mahesh-lunch-home.jpg"width="200px"height="200px" alt="">
+        <p><span style="background: orange">Ksh 1000</span></p>
+        <div class="sectioniconse">
+            <p>muttonbakari</p>
+            <i class="fa fa-minus"></i> <span>1</span> <i class="fa fa-plus"></i>
+            <p>+ Add to order</p>
+        </div>
+    </div>
+    <div class="sectionimageqw img-thumbnail"style="margin: 25px 10px">
+        <img src="image/Food_Differring_meal_Lunch_on_the_nature_024036_.jpg"width="200px"height="200px" alt="">
+        <p><span style="background: orange">Ksh 1000</span></p>
+        <div class="sectioniconse">
+            <p>muttonbakari</p>
+            <i class="fa fa-minus"></i> <span>1</span> <i class="fa fa-plus"></i>
+            <p>+ Add to order</p>
+        </div>
+    </div>
+    <br><br>
+    <div class="sectionimageqw img-thumbnail"style="margin: 25px 10px">
+        <img src="image/pexels-photo-103124.jpeg"width="200px"height="200px" alt="">
+        <p><span style="background: orange">Ksh 1000</span></p>
+        <div class="sectioniconse">
+            <p>muttonbakari</p>
+            <i class="fa fa-minus"></i> <span>1</span> <i class="fa fa-plus"></i>
+            <p>+ Add to order</p>
+        </div>
+    </div>
+    <div class="sectionimageqw img-thumbnail"style="margin: 25px 10px">
+        <img src="image/th%20(3)io.jpg"width="200px"height="200px" alt="">
+        <p><span style="background: orange">Ksh 1000</span></p>
+        <div class="sectioniconse">
+            <p>muttonbakari</p>
+            <i class="fa fa-minus"></i> <span>1</span> <i class="fa fa-plus"></i>
+            <p>+ Add to order</p>
+        </div>
+    </div>
+    <div class="sectionimageqw img-thumbnail"style="margin: 25px 10px">
+        <img src="image/pizza-wallpaper-20434-20945-hd-wallpapers.jpg" width="200px"height="200px" alt="">
+        <p><span style="background: orange">Ksh 1000</span></p>
+        <div class="sectioniconse">
+            <p>muttonbakari</p>
+            <i class="fa fa-minus"></i> <span>1</span> <i class="fa fa-plus"></i>
+            <p>+ Add to order</p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
